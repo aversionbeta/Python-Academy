becf845df6951bc4a6c2e36dcd578f6402e87165
David Betancur <juan.david.betancur@outlook.com>
2023-05-18 17:32:29 -0300

    category graphs

--- seaborn/categoric variables.py
+++ seaborn/categoric variables.py
@@ -4,3 +4,43 @@ import matplotlib.pyplot as plt
 
 tip=sns.load_dataset('tips')
 tip.head(2)
+sns.countplot(data=tip,x='day',hue='sex')
+plt.show()
+
+
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+tip=sns.load_dataset('tips')
+tip.head(2)
+plt.figure(figsize=(6,6))
+sns.stripplot(data=tip,x='day',hue='sex', y='total_bill',dodge=True)
+plt.show()
+
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+tip=sns.load_dataset('tips')
+tip.head(2)
+plt.figure(figsize=(6,6))
+sns.swarmplot(data=tip,x='day',hue='sex', y='total_bill',dodge=True)
+plt.show()
+
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+tip=sns.load_dataset('tips')
+tip.head(2)
+plt.figure(figsize=(6,6))
+sns.boxplot(data=tip,x='day',hue='sex', y='total_bill',dodge=True)
+plt.show()
+
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+tip=sns.load_dataset('tips')
+tip.head(2)
+plt.figure(figsize=(6,6))
+sns.boxplot(data=tip,x='day',hue='sex', y='total_bill',dodge=True)
+sns.swarmplot(data=tip,x='day',hue='sex', y='total_bill',dodge=True)
+plt.show()
