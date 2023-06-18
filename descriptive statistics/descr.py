0ee846db6a9ce00080325bc56cdda7d00b240dbc
David Betancur <juan.david.betancur@outlook.com>
2023-06-18 09:50:24 -0300

    Covariance and PCA analysis

--- descriptive statistics/descr.py
+++ descriptive statistics/descr.py
@@ -108,3 +108,64 @@ pd.get_dummies(df['engine_type']) #onehot
 encoder = skpre.OneHotEncoder(handle_unknown='ignore')
 encoder.fit(df[['engine_type']].values)
 encoder.transform([['gasoline'],['diesel'],['aceite']]).toarray()
+
+#Covariance Matrix
+
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.preprocessing import StandardScaler
+
+iris = sns.load_dataset('iris')
+iris.columns
+scaler = StandardScaler()
+scaled = scaler.fit_transform(iris[['sepal_length', 'sepal_width', 'petal_length', 'petal_width']])
+scaled.T
+covariance_matrix = np.cov(scaled.T)
+print(covariance_matrix)
+plt.figure(figsize=(10,10))
+sns.set(font_scale=1.5)
+hm = sns.heatmap(covariance_matrix,
+                    cbar=True,
+                    annot=True,
+                    square=True,
+                    fmt='.2f',
+                    annot_kws={'size':12},
+                    yticklabels=['sepal_length', 'sepal_width', 'petal_length', 'petal_width'],
+                    xticklabels=['sepal_length', 'sepal_width', 'petal_length', 'petal_width'])
+plt.show()
+
+#Principal Components Analysis PCA 
+
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
+
+iris = sns.load_dataset('iris')
+iris.columns
+scaler = StandardScaler()
+scaled = scaler.fit_transform(iris[['sepal_length', 'sepal_width', 'petal_length', 'petal_width']])
+scaled.T
+covariance_matrix = np.cov(scaled.T)
+print(covariance_matrix)
+sns.jointplot(x=iris['petal_length'], y=iris['petal_width'])
+sns.jointplot(x=scaled[:,2], y=scaled[:,3])
+eigen_values, eigen_vectors = np.linalg.eig(covariance_matrix)
+print(eigen_values)
+print(eigen_vectors)
+variance_explained = []
+for i in eigen_values:
+    variance_explained.append((i/sum(eigen_values))*100)
+print(variance_explained)
+
+pca = PCA(n_components=2)
+pca.fit(scaled)
+pca.explained_variance_ratio_
+reduced_scaled = pca.transform(scaled)
+print(reduced_scaled)
+iris['pca_1'] = scaled[:,0]
+iris['pca_2'] = scaled[:,1]
+sns.jointplot(iris['pca_1'],iris['pca_2'], hue = iris['species'])
+plt.show()
