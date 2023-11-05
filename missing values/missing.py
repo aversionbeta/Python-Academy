253f5f302268c6727e1fc80a684956b0133cda81
David Betancur <juan.david.betancur@outlook.com>
2023-11-05 13:49:25 -0500

    load dataframe

---
+++ missing values/missing.py
@@ -0,0 +1,9 @@
+import janitor
+import numpy as np
+import pandas as pd
+import missingno
+
+df = pd.read_csv(r"C:\Users\juand\Dropbox\Profesional\7. Delphus Lab\5. Delphus Lab Academy\Python-Academy\missing values\amazon.csv")
+df
+
+df.shape()
